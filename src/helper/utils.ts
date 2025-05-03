836534a733e2d648d8aafa0f0795f27d46dba2a5
saiyed Rahil <rahilsaiyed1711@gmail.com>
2025-05-03 13:01:03 +0530

    rough commit

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -7,4 +7,12 @@ function generate(){
     return password
 }
 
-export {generate} 
+function generatePassword(){
+    let password = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890'
+    for(let i=0;i<8;i++){
+        password += possible.charAt(Math.floor(Math.random()*possible.length));
+    }
+    return password
+}
+export {generate, generatePassword} 
